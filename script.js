96a8886b93af700865186a7f187f06f946cff1e9
MahekPandya <58770031+MahekPandya@users.noreply.github.com>
2020-06-17 05:10:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+var names = ["Rahul", "John", "Nikhil","Jen", "Jason","Prashant","Jim","Laura"];
+
+
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
